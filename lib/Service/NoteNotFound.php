117817352aecad10a1c6025bdeafa57a49a44ad6
Bruno Alfred <brunoalfred@Brunos-MacBook-Pro.local>
2022-10-20 10:11:42 +0300

    first commit

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Bruno Alfred <hello@brunoalfred.me>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\SignUp\Service;
+
+class NoteNotFound extends \Exception {
+}

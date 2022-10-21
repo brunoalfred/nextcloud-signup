335c0178ac892df39a72571ac8a40acac0f3d6f4
Bruno Alfred <brunoalfred@Brunos-MacBook-Pro.local>
2022-10-21 10:35:23 +0300

    WIP: clean the default template and start working

--- lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Bruno Alfred <hello@brunoalfred.me>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\TwigacloudSignup\Service;
-
-class NoteNotFound extends \Exception {
-}

9b438d5d8df697b18e5645fd79d4542a0c61f989
Bruno Alfred <alfredybruno@gmail.com>
2022-10-24 13:30:05 +0300

    fix: namespacing

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,25 +1,27 @@
 <?php
+
 declare(strict_types=1);
 // SPDX-FileCopyrightText: Bruno Alfred <hello@brunoalfred.me>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-namespace OCA\TwigacloudSignup\AppInfo;
+namespace OCA\Twigacloudsignup\AppInfo;
 
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App implements IBootstrap {
+class Application extends App implements IBootstrap
+{
 	public const APP_ID = 'twigacloudsignup';
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct(self::APP_ID);
 	}
 
 	public function register(IRegistrationContext $context): void
 	{
-
 	}
 
 	public function boot(IBootContext $context): void

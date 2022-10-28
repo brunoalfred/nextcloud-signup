7a37eba8b06386aec1bc151fa2dbd774b5198fcd
Bruno Alfred <alfredybruno@gmail.com>
2022-10-28 08:22:13 +0300

    fix: verification component

--- src/components/Verification.vue
+++ src/components/Verification.vue
@@ -20,29 +20,36 @@
   -
   -->
 <template>
-    <div class="guest-box">
-        <form action="" method="post">
-            <fieldset>
-                <NcNoteCard v-if="message !== ''" type="error">
-                    {{ message }}
-                </NcNoteCard>
+	<div class="guest-box">
+		<form action="" method="post">
+			<fieldset>
+				<NcNoteCard v-if="message !== ''" type="error">
+					{{ message }}
+				</NcNoteCard>
 
-                <NcTextField type="text" name="token" :label="t('registration', 'Verification code')"
-                    :label-visible="true" required autofocus>
-                    <ShieldCheck :size="20" />
-                </NcTextField>
+				<NcTextField type="text"
+					name="token"
+					:label="t('twigacloudsignup', 'Verification code')"
+					:label-visible="true"
+					required
+					autofocus>
+					<ShieldCheck :size="20" />
+				</NcTextField>
 
-                <input type="hidden" name="requesttoken" :value="requesttoken">
-                <NcButton id="submit" native-type="submit" type="primary" :wide="true">
-                    {{ t('registration', 'Verify') }}
-                </NcButton>
+				<input type="hidden" name="requesttoken" :value="requesttoken">
+				<NcButton id="submit"
+					native-type="submit"
+					type="primary"
+					:wide="true">
+					{{ t('twigacloudsignup', 'Verify') }}
+				</NcButton>
 
-                <NcButton type="tertiary" :href="loginFormLink" :wide="true">
-                    {{ t('registration', 'Back to login') }}
-                </NcButton>
-            </fieldset>
-        </form>
-    </div>
+				<NcButton type="tertiary" :href="loginFormLink" :wide="true">
+					{{ t('twigacloudsignup', 'Back to login') }}
+				</NcButton>
+			</fieldset>
+		</form>
+	</div>
 </template>
 
 <script>
@@ -54,22 +61,22 @@ import NcNoteCard from '@nextcloud/vue/dist/Components/NcNoteCard.js'
 import NcTextField from '@nextcloud/vue/dist/Components/NcTextField.js'
 
 export default {
-    name: 'Verification',
+	name: 'Verification',
 
-    components: {
-        NcButton,
-        NcTextField,
-        NcNoteCard,
-        ShieldCheck,
-    },
+	components: {
+		NcButton,
+		NcTextField,
+		NcNoteCard,
+		ShieldCheck,
+	},
 
-    data() {
-        return {
-            message: loadState('registration', 'message'),
-            requesttoken: getRequestToken(),
-            loginFormLink: loadState('registration', 'loginFormLink'),
-        }
-    },
+	data() {
+		return {
+			message: loadState('twigacloudsignup', 'message'),
+			requesttoken: getRequestToken(),
+			loginFormLink: loadState('twigacloudsignup', 'loginFormLink'),
+		}
+	},
 }
 </script>
 
